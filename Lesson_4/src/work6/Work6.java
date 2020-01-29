d5f19c46dcca6643510824bb13875a4d59be8d7b
Goo <1diaamond@gmail.com>
2020-01-29 18:03:15 +0200

    hometasks

---
+++ Lesson_4/src/work6/Work6.java
@@ -0,0 +1,22 @@
+package work6;
+
+import java.util.Scanner;
+
+public class Work6 {
+    public static void main(String[] args) {
+        System.out.println("Введите число ==>");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int result = 0;
+        if (x < 0) {
+            x*=(-1);
+        }
+        while (x > 0) {
+            int y = x % 10;
+            x /= 10;
+            y*=y;
+            result += y;
+        }
+        System.out.println(result);
+    }
+}
