4c046444652978d7782b91ae959f6fd0076c7886
Goo <1diaamond@gmail.com>
2020-01-29 18:45:57 +0200

    hometasks

--- Lesson_4/src/com/al/work6/Work6.java
+++ Lesson_4/src/com/al/work6/Work6.java
@@ -8,8 +8,9 @@ public class Work6 {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int result = 0;
+        // x = x < 0 ? -x: x; терн. операция
         if (x < 0) {
-            x*=(-1);
+            x=-x;
         }
         while (x > 0) {
             int y = x % 10;
