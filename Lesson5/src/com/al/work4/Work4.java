9dd48ea261d7def9af65845d0b2ea92779bf4fb6
Goo <1diaamond@gmail.com>
2020-01-31 00:12:43 +0200

    hometask

--- Lesson5/src/com/al/work4/Work4.java
+++ Lesson5/src/com/al/work4/Work4.java
@@ -1,8 +1,10 @@
 package com.al.work4;
 
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Work4 {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         String str1 = "Cartoon";
         String str2 = "Tomcat";
         int size = str1.length();
@@ -13,6 +15,19 @@ public class Work4 {
                 System.out.print(symbol + " ");
             }
             index++;
+        }
+    }*/
+    public static void main(String[] args) {
+        String myStr1 = "Cartoon";
+        String myStr2 = "Tomcat";
+        int index = myStr1.length();
+        int i = 0;
+        while (i < index){
+            char letter = myStr1.charAt(i);
+            if (myStr2.indexOf(letter) == -1) {
+                System.out.print(letter);
+            }
+            i++;
         }
     }
 }
