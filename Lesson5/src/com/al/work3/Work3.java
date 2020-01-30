5416d326964120012e624d69856ecd1bfb388175
Goo <1diaamond@gmail.com>
2020-01-30 22:19:53 +0200

    hometask

--- Lesson5/src/com/al/work3/Work3.java
+++ Lesson5/src/com/al/work3/Work3.java
@@ -1,7 +1,7 @@
 package com.al.work3;
 
 public class Work3 {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         String text =  "An information system is designed to collect, process, store and distribute information";
         int position1 = text.indexOf(" ");
         int position2 = text.lastIndexOf(" ");
@@ -9,5 +9,12 @@ public class Work3 {
                 text.substring(0,position1);
         System.out.println(result);
 
+    }*/
+    public static void main(String[] args) {
+        String text = "An information system is designed to collect, process, store and distribute information";
+        String word1 = text.substring(0, text.indexOf(' '));
+        String word2 = text.substring(text.lastIndexOf(' '));
+        String sentense = text.substring(text.indexOf(' '),(text.lastIndexOf(' ')));
+        System.out.println(word2 + sentense + word1);
     }
 }
