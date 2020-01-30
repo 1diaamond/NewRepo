7ee5e7012b9dd683c25151608023b48bf3d13755
Goo <1diaamond@gmail.com>
2020-01-30 21:23:49 +0200

    hometask

---
+++ Lesson5/src/com/al/work1/Work1.java
@@ -0,0 +1,15 @@
+package com.al.work1;
+
+
+public class Work1 {
+
+    public static void main(String[] args) {
+	String  string = "The decimal system, invented in " +
+            "India around AD 600, was a revolution in " +
+            "quantitative reasoning: using only 10 " +
+            "symbols, even very large numbers could be " +
+            "written down compactly";
+
+        System.out.println(string.substring(string.length() / 2) );
+    }
+}
